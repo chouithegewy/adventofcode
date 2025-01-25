6bc21cce23ba2635d699cd3e6a4b3626916abae8
chouithegewy <combatburrito@gmail.com>
2025-01-24 23:30:08 -0800

    Gold star pt2

--- 2024/java/day1/DayOne.java
+++ 2024/java/day1/DayOne.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
 
@@ -16,20 +17,28 @@ public class DayOne {
             ArrayList<Integer> col1 = new ArrayList<Integer>();
             ArrayList<Integer> col2 = new ArrayList<Integer>();
             ArrayList<Integer> d = new ArrayList<Integer>();
-            int count = 0;
+            //int count = 0;
             while ((s = bf.readLine()) != null) {
                 col1.add(Integer.parseInt(s.split("\\s+")[0]));
                 col2.add(Integer.parseInt(s.split("\\s+")[1]));
             }
             List<Integer> col1_stream = col1.stream().sorted().collect(Collectors.toList());
             List<Integer> col2_stream = col2.stream().sorted().collect(Collectors.toList());
+            HashMap<Integer, Integer> similarities = new HashMap<Integer, Integer>();
             long sum = 0;
             for (int i = 0; i < 1000; i++) {
-                //d.add(col1_stream.get(i) - col2_stream.get(i));
-                sum += Math.abs(col1_stream.get(i) - col2_stream.get(i));
-                if (i < 10) {
-                    System.out.println(
-                            col1_stream.get(i).toString() + " + " + col2_stream.get(i).toString() + " = " + sum);
+                Integer key = col2.get(i);
+                Integer count = similarities.get(key);
+                if (count == null) {
+                    similarities.put(key, 1);
+                } else {
+                    similarities.put(key, count + 1);
+                }
+            }
+            for (int v : col1) {
+                Integer sims = similarities.get(v);
+                if (sims != null) {
+                    sum += (v * sims);
                 }
             }
             System.out.println(sum);
